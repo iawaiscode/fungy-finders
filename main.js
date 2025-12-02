a663140cf1ae0801b907c23d2bdc5666879aa77b
iawaiscode <iawaiscode@gmail.com>
2025-12-02 07:58:06 +0400

    add second page

---
+++ main.js
@@ -0,0 +1,16 @@
+const navToggle = document.querySelector(
+  '[aria-controls="primary-navigation"]',
+);
+const primaryNav = document.querySelector("#primary-navigation");
+
+navToggle.addEventListener("click", function toggleMenu() {
+  const isOpened = navToggle.getAttribute("aria-expanded");
+
+  if (isOpened === "false") {
+    navToggle.setAttribute("aria-expanded", "true");
+    // primaryNav.classList.remove("visually-hidden");
+  } else {
+    navToggle.setAttribute("aria-expanded", "false");
+    // primaryNav.classList.add("visually-hidden");
+  }
+});
